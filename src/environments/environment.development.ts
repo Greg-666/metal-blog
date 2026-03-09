19261e29c1994a06f16d691012a4832e7c870bd3
greg <pqbf@live.be>
2026-03-09 11:02:28 +0100

    fix: environment.ts pointe vers backend Render

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
-  apiUrl:  'https://metal-blog-backend.onrender.com'
+  production: true,
+  apiUrl: 'https://metal-blog-backend.onrender.com'
 };
